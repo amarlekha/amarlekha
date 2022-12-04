c4d2083bd60f8a08d71cbc2a0f9869c95853563a
amarlekha <119776392+amarlekha@users.noreply.github.com>
2022-12-04 14:48:19 +0530

    Update main.js

--- dmug/main.js
+++ dmug/main.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
+        $('#nav-bar-icon').toggleClass("active");
     });
    
 });
